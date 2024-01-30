d7d54dbfa1faa8acfd116f347f741184dec7b232
hkkrk <hamza.karakaya77@outlook.com>
2024-01-30 17:58:56 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/swapease/ui/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/example/swapease/ui/viewmodels/UserViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.swapease.data.repositories.UserRepository
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
